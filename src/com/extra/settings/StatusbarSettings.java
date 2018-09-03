53fbe4e39e555c7c3290a782755e444f20e000ba
ezio84 <brabus84@gmail.com>
2018-09-03 22:03:08 +0700

    Status network traffic [2/2]

--- src/com/extra/settings/StatusbarSettings.java
+++ src/com/extra/settings/StatusbarSettings.java
@@ -16,55 +16,74 @@
 
 package com.extra.settings;
 
-import android.content.Context;
-import android.content.ContentResolver;
-import android.content.Intent;
-import android.content.pm.UserInfo;
 import android.os.Bundle;
 import android.os.UserHandle;
-import android.os.UserManager;
-import android.support.v7.preference.ListPreference;
+import android.provider.Settings;
+import android.content.ContentResolver;
+import android.support.v4.app.Fragment;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceCategory;
 import android.support.v7.preference.PreferenceScreen;
+import android.support.v7.preference.PreferenceFragmentCompat;
 import android.support.v7.preference.Preference.OnPreferenceChangeListener;
-import android.support.v14.preference.SwitchPreference;
-import android.provider.Settings;
-import com.android.settings.R;
-import android.support.annotation.NonNull;
 
-import com.android.internal.logging.nano.MetricsProto;
+import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 
-import java.util.Arrays;
-import java.util.ArrayList;
-import java.util.List;
+import com.extra.settings.preferences.SystemSettingSwitchPreference;
+import com.extra.settings.preferences.CustomSeekBarPreference;
 
+import com.android.internal.logging.nano.MetricsProto;
 
- public class StatusbarSettings extends SettingsPreferenceFragment
-                implements Preference.OnPreferenceChangeListener {
+public class StatusbarSettings extends SettingsPreferenceFragment implements OnPreferenceChangeListener{
 
+		private CustomSeekBarPreference mThreshold;
+		private SystemSettingSwitchPreference mNetMonitor;
 
      @Override
-    public void onCreate(Bundle icicle) {
-        super.onCreate(icicle);
+	 public void onCreate(Bundle bundle) {
+		super.onCreate(bundle);
+		addPreferencesFromResource(R.xml.extra_settings_statusbar);
 
-         addPreferencesFromResource(R.xml.extra_settings_statusbar);
+         PreferenceScreen prefSet = getPreferenceScreen();
 
-         final ContentResolver resolver = getActivity().getContentResolver();
-        final PreferenceScreen prefScreen = getPreferenceScreen();
+ 		final ContentResolver resolver = getActivity().getContentResolver();
+		boolean isNetMonitorEnabled = Settings.System.getIntForUser(resolver,
+		    Settings.System.NETWORK_TRAFFIC_STATE, 1, UserHandle.USER_CURRENT) == 1;
+		mNetMonitor = (SystemSettingSwitchPreference) findPreference("network_traffic_state");
+		mNetMonitor.setChecked(isNetMonitorEnabled);
+		mNetMonitor.setOnPreferenceChangeListener(this);
+		int value = Settings.System.getIntForUser(resolver,
+		    Settings.System.NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD, 1, UserHandle.USER_CURRENT);
+		mThreshold = (CustomSeekBarPreference) findPreference("network_traffic_autohide_threshold");
+		mThreshold.setValue(value);
+		mThreshold.setOnPreferenceChangeListener(this);
+		mThreshold.setEnabled(isNetMonitorEnabled);
 
      }
 
-     @Override
-    public boolean onPreferenceChange(Preference preference, Object newValue) {
-
-         return false;
-    }
+	@Override
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+         if (preference == mNetMonitor) {
+            boolean value = (Boolean) objValue;
+            Settings.System.putIntForUser(getActivity().getContentResolver(),
+                    Settings.System.NETWORK_TRAFFIC_STATE, value ? 1 : 0,
+                    UserHandle.USER_CURRENT);
+            mNetMonitor.setChecked(value);
+            mThreshold.setEnabled(value);
+            return true;
+        } else if (preference == mThreshold) {
+            int val = (Integer) objValue;
+            Settings.System.putIntForUser(getContentResolver(),
+                    Settings.System.NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD, val,
+                    UserHandle.USER_CURRENT);
+            return true;
+        }
+        return false;
+}
 
      @Override
     public int getMetricsCategory() {
         return MetricsProto.MetricsEvent.AOSP;
     }
-
- }
+}
