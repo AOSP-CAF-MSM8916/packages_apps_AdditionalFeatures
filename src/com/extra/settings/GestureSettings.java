d7d71e33c8e5ae9107032d5626e3558c37f55762
Sebastian <kjjjnob.seba@gmail.com>
2019-01-30 02:50:57 -0300

    Fix build
    Signed-off-by: Sebastian <kjjjnob.seba@gmail.com>

--- src/com/extra/settings/GestureSettings.java
+++ src/com/extra/settings/GestureSettings.java
@@ -1,88 +1,63 @@
-package com.extra.settings;
+/*
+ *  Copyright (C) 2016 The Dirty Unicorns Project
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 
-import com.android.internal.logging.nano.MetricsProto;
+ package com.extra.settings;
 
+import android.content.ContentResolver;
+import android.content.res.Resources;
 import android.content.Context;
 import android.os.Bundle;
-import android.provider.Settings;
-import android.provider.SearchIndexableResource;
-import android.support.v7.preference.Preference;
+import android.os.UserHandle;
+import android.os.Vibrator;
+import android.support.v7.preference.PreferenceCategory;
 import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceScreen;
+import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+import android.support.v14.preference.SwitchPreference;
+import android.provider.Settings;
 
-import com.android.settings.R;
+ import com.android.settings.R;
 
-import com.android.settings.search.BaseSearchIndexProvider;
-import com.android.settings.search.Indexable;
-import com.android.settings.SettingsPreferenceFragment;
+ import com.android.settings.SettingsPreferenceFragment;
 
-import java.util.ArrayList;
-import java.util.List;
+ import com.android.internal.logging.nano.MetricsProto;
 
-public class NosGestureSettings extends SettingsPreferenceFragment implements
-                          Preference.OnPreferenceChangeListener, Indexable {
+ public class GestureSettings extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener{
 
-    private static final String KEY_TORCH_LONG_PRESS_POWER_TIMEOUT =
-            "torch_long_press_power_timeout";
-
-    private ListPreference mTorchLongPressPowerTimeout;
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
+     @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
         addPreferencesFromResource(R.xml.extra_settings_gestures);
 
-        mTorchLongPressPowerTimeout =
-                    (ListPreference) findPreference(KEY_TORCH_LONG_PRESS_POWER_TIMEOUT);
-
-        mTorchLongPressPowerTimeout.setOnPreferenceChangeListener(this);
-        int TorchTimeout = Settings.System.getInt(getContentResolver(),
-                        Settings.System.TORCH_LONG_PRESS_POWER_TIMEOUT, 0);
-        mTorchLongPressPowerTimeout.setValue(Integer.toString(TorchTimeout));
-        mTorchLongPressPowerTimeout.setSummary(mTorchLongPressPowerTimeout.getEntry());
+         final PreferenceScreen prefScreen = getPreferenceScreen();
     }
 
-    public boolean onPreferenceChange(Preference preference, Object newValue) {
-         if (preference == mTorchLongPressPowerTimeout) {
-            String TorchTimeout = (String) newValue;
-            int TorchTimeoutValue = Integer.parseInt(TorchTimeout);
-            Settings.System.putInt(getActivity().getContentResolver(),
-                    Settings.System.TORCH_LONG_PRESS_POWER_TIMEOUT, TorchTimeoutValue);
-            int TorchTimeoutIndex = mTorchLongPressPowerTimeout
-                    .findIndexOfValue(TorchTimeout);
-            mTorchLongPressPowerTimeout
-                    .setSummary(mTorchLongPressPowerTimeout.getEntries()[TorchTimeoutIndex]);
-            return true;
-        }
-        return false;
+     public boolean onPreferenceChange(Preference preference, Object newValue) {
+        ContentResolver resolver = getActivity().getContentResolver();
+
+         return false;
     }
 
-    @Override
+     @Override
     public int getMetricsCategory() {
         return MetricsProto.MetricsEvent.AOSP;
     }
 
-    /**
-     * For Search.
-     */
-    public static final SearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
-            new BaseSearchIndexProvider() {
-
-                @Override
-                public List<SearchIndexableResource> getXmlResourcesToIndex(Context context,
-                        boolean enabled) {
-                    final ArrayList<SearchIndexableResource> result = new ArrayList<>();
-
-                    final SearchIndexableResource sir = new SearchIndexableResource(context);
-                    sir.xmlResId = R.xml.extra_settings_gestures;
-                    result.add(sir);
-                    return result;
-                }
-
-                @Override
-                public List<String> getNonIndexableKeys(Context context) {
-                    final List<String> keys = super.getNonIndexableKeys(context);
-                    return keys;
-                }
-    };
-}
+ }
